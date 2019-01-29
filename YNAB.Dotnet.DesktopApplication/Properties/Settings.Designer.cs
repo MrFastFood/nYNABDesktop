9dc148d453aa42a6d111a7957e7337edb8379081
Ashley B <ashley.begley@pm.me>
2019-01-29 23:27:34 +0000

    Fixed AutoUpdater templates

--- YNAB.Dotnet.DesktopApplication/Properties/Settings.Designer.cs
+++ YNAB.Dotnet.DesktopApplication/Properties/Settings.Designer.cs
@@ -8,22 +8,28 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace YNAB.Dotnet.DesktopApplication.Properties
-{
-
-
+namespace YNAB.Dotnet.DesktopApplication.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://raw.githubusercontent.com/MrFastFood/nYNABDesktop/feature/AutoUpdater/Set" +
+            "upFiles/Alpha/config.xml")]
+        public string AutoUpdaterConfig {
+            get {
+                return ((string)(this["AutoUpdaterConfig"]));
             }
         }
     }
---
+++ YNAB.Dotnet.DesktopApplication/ViewModel/AutoUpdaterViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YNAB.Dotnet.DesktopApplication.ViewModel
+{
+    public class AutoUpdaterViewModel : IViewModel
+    {
+        #region Attribute(s)
+
+        private int _PercentageComplete = 30;
+        public int PercentageComplete
+        {
+            get { return _PercentageComplete; }
+            set
+            {
+                if (_PercentageComplete != value)
+                {
+                    _PercentageComplete = value;
+                    OnPropertyChanged(nameof(PercentageComplete));
+                }
+            }
+        }
+
+        private string _Status = "Sample status text";
+        public string Status
+        {
+            get { return _Status; }
+            set
+            {
+                if (_Status != value)
+                {
+                    _Status = value;
+                    OnPropertyChanged(nameof(Status));
+                }
+            }
+        }
+
+        #endregion
+
+        #region IViewModel Implementation
+
+        public override event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+
+        }
+
+        #endregion
+    }
+}
